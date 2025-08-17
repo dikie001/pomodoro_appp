980cadff653f49026e3dc72069d7b6a21f6bd867
dikie001 <omondidickens255@gmail.com>
2025-08-17 15:57:37 +0300

    feat:made  the app offline, changed icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,58 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base:'/',
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Focus",
+        short_name: "Focus",
+        start_url: ".",
+        display: "standalone",
+        background_color: "#162456",
+        theme_color: "#162456",
+        icons: [
+          {
+            src: "images/logo.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "images/logo.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === "document",
+            handler: "NetworkFirst",
+            options: { cacheName: "html-cache" },
+          },
+          {
+            urlPattern: ({ request }) =>
+              ["style", "script", "worker"].includes(request.destination),
+            handler: "StaleWhileRevalidate",
+            options: { cacheName: "asset-cache" },
+          },
+          {
+            urlPattern: ({ request }) => request.destination === "image",
+            handler: "CacheFirst",
+            options: {
+              cacheName: "image-cache",
+              expiration: { maxEntries: 50 },
+            },
+          },
+        ],
+      },
+    }),
+  ],
+  base: "/",
+});
